2450551bee637a9cad9efa0315bd210abb0d94ac
Dara <72565678+Dara-PP@users.noreply.github.com>
2026-02-26 02:55:07 +0100

    feat: implémentation de Room et persistance des données JSON
    Mise en place de la base de données Room (Entity, DAO, Database).
    
    Ajout d'une logique d'initialisation : chargement depuis Room si présent, sinon injection depuis le JSON.
    
    Refactor : déplacement de DataStream.kt vers le package UI et nettoyage

---
+++ app/src/main/java/com/example/projet_android_m2/PlaceDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.projet_android_m2
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [PlacePersonality::class],
+    version = 1 // Migration pour base de donnée
+)
+// Prompt Claude Sonnet 4.6 : Donne moi le code pour initier une base de données Room en kotlin en te basant sur mes fichier fichier PlacePersonalitly.kt et PlaceDao.kt,
+// car celle de la documentation ne marche pas dans mon cas "https://developer.android.com/training/data-storage/room?hl=fr"
+abstract class PlaceDatabase: RoomDatabase() {
+    abstract fun placeDao(): PlaceDao
+
+    companion object {
+        @Volatile private var INSTANCE: PlaceDatabase? = null
+
+        fun getInstance(context: Context): PlaceDatabase {
+            return INSTANCE ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    PlaceDatabase::class.java,
+                    "places_db"
+                ).build().also { INSTANCE = it }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/projet_android_m2/PlacePersonality.kt
@@ -0,0 +1,25 @@
+package com.example.projet_android_m2
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+// nom table
+@Entity(tableName = "places")
+// tout en ? nullable au cas ou
+data class PlacePersonality(
+    val personId : Long, // id person
+
+    val personNameEn: String?, // name person
+    val personNameFr: String?,
+    val nameEn: String?, // name lieux
+    val nameFr: String?,
+
+    val relationId: Long?, // type relation person <-> lieux
+    val relationNameEn: String?, // type de relation selon id
+    val relationNameFr: String?,
+
+    val locationLat: Double, // coordonnées
+    val locationLon: Double,
+    val zone: Boolean, // si zone ou point précis je pense
+
+    @PrimaryKey val id: Long, // id du lieux
+)
