3f2e7107b037ac9ba1049a85e449b8246cfc2d5c
Dara <72565678+Dara-PP@users.noreply.github.com>
2026-02-27 23:48:47 +0100

    feat: ajout du chargement JSON chunk vers Room avec écran d'initialisation
    - Implémentation du chargement du fichier JSONL par chunks pour éviter le chargement complet en mémoire
    - Insertion progressive des données dans Room chunk
    - Ajout du composable LoadScreen avec barre de progression et gestion des erreurs
    - Adaptation de MainActivity pour attendre l'initialisation de la base avant navigation
    - Ajustements mineurs dans le Repository

--- app/src/main/java/com/example/projet_android_m2/MainActivity.kt
+++ app/src/main/java/com/example/projet_android_m2/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -12,7 +16,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.projet_android_m2.ui.auth.LoginPage
 import com.example.projet_android_m2.data.KtorServer
-import com.example.projet_android_m2.ui.HomePage
+import com.example.projet_android_m2.ui.LoadScreen
 import com.example.projet_android_m2.ui.NavigationBarUI
 import com.example.projet_android_m2.ui.auth.RegisterPage
 
@@ -27,21 +31,31 @@ class MainActivity : ComponentActivity() {
             val token = server.getToken(context)
             // check si utilisateur login ou pas
             var page = if (token != null) "home" else "login"
+            // Etat du chargement
+            var isLoadingDone by remember { mutableStateOf(false) }
 
-            NavHost(
-                navController = navController,
-                startDestination = page,
-                modifier = Modifier.padding(10.dp)
-            ){
-                composable("home"){
-                    //La bar de navigation a scaffold donc il gere lui meme ca place
-                    NavigationBarUI(navController = navController)
-                }
-                composable ("login" ){
-                    LoginPage(navController = navController)
-                }
-                composable ("register" ){
-                    RegisterPage(navController = navController)
+            if (!isLoadingDone){
+                LoadScreen(
+                    onLoadComplete = {
+                        isLoadingDone = true
+                    }
+                )
+            } else{
+                NavHost(
+                    navController = navController,
+                    startDestination = page,
+                    modifier = Modifier.padding(10.dp)
+                ){
+                    composable("home"){
+                        //La bar de navigation a scaffold donc il gere lui meme ca place
+                        NavigationBarUI(navController = navController)
+                    }
+                    composable ("login" ){
+                        LoginPage(navController = navController)
+                    }
+                    composable ("register" ){
+                        RegisterPage(navController = navController)
+                    }
                 }
             }
         }
