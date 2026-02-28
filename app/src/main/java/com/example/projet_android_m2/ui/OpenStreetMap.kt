dc891895672a88321bb437cda90392fcaa850c8b
Dara <72565678+Dara-PP@users.noreply.github.com>
2026-03-01 00:21:16 +0100

    feat: affichage des points d'intérêt depuis la base de données Room
    Intégration du PlaceRepository pour récupérer les données.
    
    Génération dynamique d'une GeoJsonSource à partir des coordonnées (lat/lon).
    
    Ajout d'une CircleLayer pour visualiser les points sur la carte MapLibre.
    
    Limitation temporaire à 1000 points pour les tests de performance.

--- app/src/main/java/com/example/projet_android_m2/ui/OpenStreetMap.kt
+++ app/src/main/java/com/example/projet_android_m2/ui/OpenStreetMap.kt
@@ -2,16 +2,75 @@ package com.example.projet_android_m2.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.projet_android_m2.PlacePersonality
+import com.example.projet_android_m2.data.PlaceRepository
+import kotlinx.coroutines.flow.first
+import org.maplibre.compose.expressions.dsl.const
+import org.maplibre.compose.layers.CircleLayer
 import org.maplibre.compose.map.MaplibreMap
+import org.maplibre.compose.sources.GeoJsonData
+import org.maplibre.compose.sources.rememberGeoJsonSource
 import org.maplibre.compose.style.BaseStyle
 
 @Composable
 fun OpenStreetMap (){
+    val context = LocalContext.current
+    val repo = remember { PlaceRepository(context) }
+    var places by remember { mutableStateOf<List<PlacePersonality>>(emptyList()) }
+
+    // Get les points dans places avec call vers room via getPerson()
+    LaunchedEffect(Unit) {
+        // test avec seulement 1000 pour l'instant !
+        places = repo.getPerson(offset = 0, size = 1000).first()
+    }
+
+    val featureMapJson = remember(places) {
+        """
+        {
+          "type": "FeatureCollection",
+          "features": [
+            ${places.joinToString(","){ pointMap ->
+            """
+            {
+              "type": "Feature",
+              "properties": {},
+              "geometry": {
+                "type": "Point",
+                "coordinates": [${pointMap.locationLon}, ${pointMap.locationLat}]
+              }
+            }
+            """.trimIndent()
+            }}
+          ]
+        }
+        """.trimIndent()
+    }
+
     MaplibreMap(
         modifier = Modifier.fillMaxSize(),
-        baseStyle = BaseStyle.Uri("https://tiles.openfreemap.org/styles/liberty"))
+        baseStyle = BaseStyle.Uri("https://tiles.openfreemap.org/styles/liberty")
+    ){
+        // Transformation du json en source pour maplibre
+        val source1 = rememberGeoJsonSource(
+            data = GeoJsonData.JsonString(featureMapJson)
+        )
+        CircleLayer(
+            id = "test-1",
+            source = source1,
+            radius = const(7.dp),
+            color = const(Color.Red)
+        )
+    }
 }
 
 // A run sur le telephone pas sur preview !
